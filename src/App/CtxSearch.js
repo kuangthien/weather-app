5a15b412ea7650b2a55691ddb3a925e74a605490
Thien Tran <kuangthien@gmail.com>
2021-05-02 00:14:40 +0700

    fix empty search keyword

--- src/App/CtxSearch.js
+++ src/App/CtxSearch.js
@@ -20,19 +20,24 @@ export const CtxSearchAdaptor = {
     // Step: mapping to context
     // API ref: https://www.metaweather.com/api/#location
     // TODO: transform UTC  timzone from server if they have a code of UTC/GMT for a more correct display
-    const { consolidated_weather = [], timezone, title } = json
-    const forecastData = [...consolidated_weather].map(({ max_temp, min_temp, applicable_date }) => ({
-      max_temp: Number.parseInt(max_temp),
-      min_temp: Number.parseInt(min_temp),
-      applicable_date,
-      applicable_day: dayjs(applicable_date).format('dddd'),
-    }))
 
-    return {
-      forecastData: forecastData.filter((o, i) => i > 0 && !!o), // first item is today
-      timezone,
-      title,
-    }
+    let finalRs = {}
+    try {
+      const { consolidated_weather = [], timezone, title } = json
+      const forecastData = [...consolidated_weather].map(({ max_temp, min_temp, applicable_date }) => ({
+        max_temp: Number.parseInt(max_temp),
+        min_temp: Number.parseInt(min_temp),
+        applicable_date,
+        applicable_day: dayjs(applicable_date).format('dddd'),
+      }))
+
+      finalRs = {
+        forecastData: forecastData.filter((o, i) => i > 0 && !!o), // first item is today
+        timezone,
+        title,
+      }
+    } catch (error) {}
+    return finalRs
   },
 }
 export default CtxSearch
